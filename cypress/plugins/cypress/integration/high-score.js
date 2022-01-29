d011a49223ec2d3dc5e47be0c749663e19eaba81
PaulTimothyChambers <63275905+PaulTimothyChambers@users.noreply.github.com>
2022-01-29 13:09:24 -0700

    Add cypress dependency and directory

---
+++ cypress/plugins/cypress/integration/high-score.js
@@ -0,0 +1,13 @@
+describe('Creating new high scores' () => {
+  it('should be able to save a new high score if that score is equal to or greater than the lowest score currently on the high scores board', () => {
+
+  })
+
+  it('should not be able to save a new score if that score is less than the lowest score currently on the high scores board' () => {
+
+  })
+
+  it('should be able to enter in a player name to attach to the new high score', () => {
+    
+  })
+})
