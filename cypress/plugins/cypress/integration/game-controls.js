b1f1285e9fa1421c1eb331b59501869bbb038138
PaulTimothyChambers <63275905+PaulTimothyChambers@users.noreply.github.com>
2022-01-30 00:14:25 -0700

    Refactor test

--- cypress/plugins/cypress/integration/game-controls.js
+++ cypress/plugins/cypress/integration/game-controls.js
@@ -1,12 +1,12 @@
 describe('Initial game configuration', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/')
-    cy.wait(3000)
-    cy.get('[data-cy="cell2825"]')
+      .wait(3000)
+      .get('[data-cy="cell2825"]')
       .click()
   })
 
-  it('should be able to manually inactive toggle cells into an active state', () => {
+  it('should be able to manually toggle inactive cells into an active state', () => {
     cy.get('[data-test="cell2825a"]')
       .should('not.have.class', 'hidden')
   })
@@ -14,7 +14,7 @@ describe('Initial game configuration', () => {
   it('should be able to manually toggle active cells into an inactive state', () => {
     cy.get('[data-test="cell2825a"]')
       .click()
-    cy.get('[data-test="cell2825a"]')
+      .get('[data-test="cell2825a"]')
       .should('have.class', 'hidden')
   })
 })
@@ -22,47 +22,47 @@ describe('Initial game configuration', () => {
 describe('Game controls', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/')
-    cy.wait(3000)
-    cy.get('[data-cy="cell2825"]')
+      .wait(3000)
+      .get('[data-cy="cell2825"]')
       .click()
-    cy.get('[data-cy="cell2826"]')
+      .get('[data-cy="cell2826"]')
       .click()
-    cy.get('[data-cy="cell2924"]')
+      .get('[data-cy="cell2924"]')
       .click()
-    cy.get('[data-cy="cell3025"]')
+      .get('[data-cy="cell3025"]')
       .click()
-    cy.get('[data-cy="cell3026"]')
+      .get('[data-cy="cell3026"]')
       .click()
-    cy.get('[data-cy="cell2927"]')
+      .get('[data-cy="cell2927"]')
       .click()
-    cy.get('[data-cy="cell2925"]')
+      .get('[data-cy="cell2925"]')
       .click()
-    cy.get('[data-cy="beginGame"]')
+      .get('[data-cy="beginGame"]')
       .click()
   })
 
   it('should be able to begin the game, once one or more cells have been manually toggled to their active state', () => {
     cy.get('[data-test="cell2824a"]')
       .should('not.have.class', 'hidden')
-    cy.get('[data-test="cell3024a"]')
+      .get('[data-test="cell3024a"]')
       .should('not.have.class', 'hidden')
   })
 
   it('should be able to pause the game', () => {
     cy.get('[data-test="cell2824a"]')
       .should('not.have.class', 'hidden')
-    cy.get('[data-cy="beginGame"]')
+      .get('[data-cy="beginGame"]')
       .click()
-    cy.get('[data-test="cell2925a"]')
+      .get('[data-test="cell2925a"]')
       .should('have.class', 'hidden')
   })
 
   it('should be able to end the game and clear the board of all currently active cells', () => {
     cy.get('[data-cy="endGame"]')
       .click()
-    cy.get('[data-test="cell2925a"]')
+      .get('[data-test="cell2925a"]')
       .should('be.visible')
-    cy.get('[data-test="cell2924a"]')
+      .get('[data-test="cell2924a"]')
       .should('have.class', 'hidden')
   })
 })
